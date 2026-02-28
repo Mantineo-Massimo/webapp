2b401ea5fde6f4201751182f5c51e0b67a2fe7d8
Massimo Mantineo <mantineomassimo2003@gmail.com>
2026-02-28 12:53:19 +0100

    fix

--- src/app/leaderboards/page.tsx
+++ src/app/leaderboards/page.tsx
@@ -198,7 +198,7 @@ export default function LeaderboardsPage() {
                 {/* Modal Dettaglio Squadra */}
                 <AnimatePresence>
                     {selectedTeam && (
-                        <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+                        <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
                             <motion.div
                                 initial={{ opacity: 0 }}
                                 animate={{ opacity: 1 }}
@@ -290,7 +290,7 @@ export default function LeaderboardsPage() {
                 {/* Modal Dettaglio Artista (Eventi) */}
                 <AnimatePresence>
                     {selectedArtist && (
-                        <div className="fixed inset-0 z-[60] flex items-center justify-center p-4">
+                        <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
                             <motion.div
                                 initial={{ opacity: 0 }}
                                 animate={{ opacity: 1 }}
